c5e88e44d1aae8d278c81b0d889377ab627cf445
Yeliz Yakar <yyakar@deloitte.com>
2023-12-04 15:56:42 +1100

    ISR feature with API

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,36 @@
-export default async function About() {
-  return <main>About page</main>;
+// export default async function About() {
+//   return <main>About page</main>;
+// }
+import React from "react";
+
+async function getData() {
+  const res = await fetch("https://api.publicapis.org/entries");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <main>
+      <p>Amount of data: {data.count}</p>
+
+      <>
+        {data.entries.map((data: any, key: any) => (
+          <p key={data}>
+            <>{data.API + ": "}</>
+            <>{data.Description}</>
+          </p>
+        ))}
+      </>
+    </main>
+  );
 }
